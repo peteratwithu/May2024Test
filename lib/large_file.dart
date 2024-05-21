2e7dbf1a9b5f20672bc57d15298a3d427afb85e8
peteratwithu <130485614+peteratwithu@users.noreply.github.com>
2024-05-21 18:37:55 +0100

    Expand large_file.dart with additional methods

--- lib/large_file.dart
+++ lib/large_file.dart
@@ -19,20 +19,4 @@ class LargeClassA extends LargeFile {
   static void method3() {
     print("Method 3 in LargeClassA");
   }
-  // ... add more methods as needed to increase the size
-}
-
-class LargeClassB extends LargeFile {
-  static void method1() {
-    print("Method 1 in LargeClassB");
-  }
-
-  static void method2() {
-    print("Method 2 in LargeClassB");
-  }
-
-  static void method3() {
-    print("Method 3 in LargeClassB");
-  }
-  // ... add more methods as needed to increase the size
 }
