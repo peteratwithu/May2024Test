4bfbc6ef72eb67522beedb0401509ea20f69b94f
peteratwithu <130485614+peteratwithu@users.noreply.github.com>
2024-05-21 18:42:00 +0100

    Expand large_file.dart with more methods and new classes

--- lib/large_file.dart
+++ lib/large_file.dart
@@ -7,76 +7,6 @@ class LargeFile {
   }
 }
 
-class LargeClassA extends LargeFile {
-  static void method1() {
-    print("Method 1 in LargeClassA");
-  }
-
-  static void method2() {
-    print("Method 2 in LargeClassA");
-  }
-
-  static void method3() {
-    print("Method 3 in LargeClassA");
-  }
-  static void method4() {
-    print("Method 4 in LargeClassA");
-  }
-  static void method5() {
-    print("Method 5 in LargeClassA");
-  }
-  static void method6) {
-    print("Method 6 in LargeClassA");
-  }
-  static void method7() {
-    print("Method 7 in LargeClassA");
-  }
-  static void method8() {
-    print("Method 8 in LargeClassA");
-  }
-  static void method9() {
-    print("Method 9 in LargeClassA");
-  }
-  static void method10() {
-    print("Method 10 in LargeClassA");
-  }
-}
-
-class LargeClassB extends LargeFile {
-  static void method1() {
-    print("Method 1 in LargeClassB");
-  }
-
-  static void method2() {
-    print("Method 2 in LargeClassB");
-  }
-
-  static void method3() {
-    print("Method 3 in LargeClassB");
-  }
-  static void method4() {
-    print("Method 4 in LargeClassB");
-  }
-  static void method5() {
-    print("Method 5 in LargeClassB");
-  }
-  static void method6() {
-    print("Method 6 in LargeClassB");
-  }
-  static void method7() {
-    print("Method 7 in LargeClassB");
-  }
-  static void method8() {
-    print("Method 8 in LargeClassB");
-  }
-  static void method9() {
-    print("Method 9 in LargeClassB");
-  }
-  static void method10() {
-    print("Method 10 in LargeClassB");
-  }
-}
-
 class LargeClassC extends LargeFile {
   static void method1() {
     print("Method 1 in LargeClassC");
@@ -88,5 +18,40 @@ class LargeClassC extends LargeFile {
 
   static void method3() {
     print("Method 3 in LargeClassC");
+  }
+  static void method4() {
+    print("Method 4 in LargeClassC");
+  }
+  static void method5() {
+    print("Method 5 in LargeClassC");
+  }
+  static void method6) {
+    print("Method 6 in LargeClassC");
+  }
+  static void method7() {
+    print("Method 7 in LargeClassC");
+  }
+  static void method8() {
+    print("Method 8 in LargeClassC");
+  }
+  static void method9() {
+    print("Method 9 in LargeClassC");
+  }
+  static void method10() {
+    print("Method 10 in LargeClassC");
+  }
+}
+
+class LargeClassD extends LargeFile {
+  static void method1() {
+    print("Method 1 in LargeClassD");
+  }
+
+  static void method2() {
+    print("Method 2 in LargeClassD");
+  }
+
+  static void method3() {
+    print("Method 3 in LargeClassD");
   }
 }
